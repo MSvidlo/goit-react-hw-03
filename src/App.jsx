1976f4bf303919243c97a687702093f663c26ab9
MSvidlo <143841475+MSvidlo@users.noreply.github.com>
2024-04-12 15:14:32 +0200

    adding new contacts

--- src/App.jsx
+++ src/App.jsx
@@ -9,32 +9,31 @@ import { useEffect } from 'react';
 import contacts from './contacts.json'
 import * as Yup from "yup";
 import { Formik, Form, Field  } from 'formik';
+import initialContacts from './contacts.json';
 
 const App = () => {
-
+  const [contacts, setContacts] = useState(initialContacts);
   const [searchValue, setSearchValue] = useState('');
 
   const handleChangeSearch = (event) => {
-     setSearchValue(event.target.value);
+    setSearchValue(event.target.value);
+  }
+
+  const addContact = (newContact) => {
+    setContacts([...contacts, newContact]);
   }
 
   const filterContacts = contacts.filter((contact) => 
     contact.name.toLowerCase().includes(searchValue.toLowerCase()));
 
-
-    return (
-      <>
-        <div>
-          <h1>Phonebook</h1>
-          <ContactFrom />
-          <SearchForm searchValue={searchValue}
-            handleChangeSearch={handleChangeSearch}
-          />
-          <ContacList contacts={contacts} filterContacts={filterContacts} />
-        </div>
-      </>
-    );
-  };
-
+  return (
+    <div>
+      <h1>Phonebook</h1>
+      <ContactFrom addContact={addContact} />
+      <SearchForm searchValue={searchValue} handleChangeSearch={handleChangeSearch} />
+      <ContacList contacts={contacts} filterContacts={filterContacts} />
+    </div>
+  );
+};
 
 export default App
